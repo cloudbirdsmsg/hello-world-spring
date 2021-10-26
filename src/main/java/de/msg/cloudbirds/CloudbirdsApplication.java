cf9ec619ada424e9d6adadaf0ff6cdf336d29210
Nico Rogalski (msg) <nico.rogalski@msg-david.de>
2021-10-26 12:04:51 +0200

    init

---
+++ src/main/java/de/msg/cloudbirds/CloudbirdsApplication.java
@@ -0,0 +1,13 @@
+package de.msg.cloudbirds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbirdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbirdsApplication.class, args);
+	}
+
+}
